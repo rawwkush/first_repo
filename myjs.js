a5698f9fc56ea2ebec5b0ea5f8e5febf110b31d4
rawwkush <ankush.rawat@thinkitive.com>
2021-01-07 16:58:39 +0530

    auto type

---
+++ myjs.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".typed", {
+    strings: ["Android Developer.", "Web Developer.", "Programmer."],
+    typeSpeed: 70,
+    loop: true,
+    startDelay: 1000,
+    showCursor: false
+  });
