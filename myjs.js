ac05070562f096e3d795a2ca54b212bb47cf67a9
rawwkush <ankush.rawat@thinkitive.com>
2021-01-07 17:11:51 +0530

    added efw features

--- myjs.js
+++ myjs.js
@@ -1,3 +1,6 @@
+$(function() {
+
+
 var typed = new Typed(".typed", {
     strings: ["Thinkitive", "Github.", "Git","Web Dev"],
     typeSpeed: 70,
@@ -5,3 +8,13 @@ var typed = new Typed(".typed", {
     startDelay: 1000,
     showCursor: false
   });
+
+  var op = new Typed(".op", {
+    strings: ["Web DEveloper", "Android  DEveloper", "Software DEveloper","Software Trainee"],
+    typeSpeed: 70,
+    loop: true,
+    startDelay: 1000,
+    showCursor: false
+  });
+
+});
